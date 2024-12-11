001d395e3096daadce808222c13a0a7a78b2f3b8
paulapreuss <paulavpreuss@outlook.com>
2024-12-11 15:51:46 +0100

    Create `ScenarioBuilder` class to process survey answers

--- component_library/scripts/build_scenario.py
+++ component_library/scripts/build_scenario.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import logging
 
+from app.survey.survey import SURVEY_ANSWER_COMPONENT_MAPPING
 from utils import AVAILABLE_COMPONENTS, COMPONENT_TEMPLATES_PATH
 from analyse_survey import create_components_list
 
@@ -78,6 +79,53 @@ def create_scenario_from_survey_data(survey_data, scenario_name, scenario_path):
 
     # TODO read spec csv file and update the scenario values of the resources
     # TODO the specs csv file must have the same component names and component attribute names
+
+
+class ScenarioBuilder:
+    def __init__(self):
+        self.mapping = SURVEY_ANSWER_COMPONENT_MAPPING
+        self.components = {}
+
+    def process_survey(self, survey):
+        """
+        Process the survey responses to build a nested structure. Some answers add components, while some change
+        the attributes of the components. The output structure allows to change all attributes in the .csv files at
+        once without editing them multiple times.
+        Example output:
+        {
+            "wind_turbine": {"capacity": 10, "some_parameter": 5},
+            "diesel_generator": {"fuel_efficiency": 0.8}
+        }
+        """
+        for question_id, answer in survey.items():
+            if answer is not None:
+                question_id = question_id.strip("criteria_")
+
+                if question_id in self.mapping:
+                    # Check if it's a top-level question for adding components
+                    try:
+                        int_test = int(question_id)
+                        component_map = self.mapping[question_id]
+                        components_to_add = []
+                        for component in answer:
+                            if isinstance(component_map.get(component, None), list):
+                                components_to_add.extend(component_map.get(component, None))
+                            else:
+                                components_to_add.append(component_map.get(component, None))
+
+                        self.components.update({component: {} for component in components_to_add})
+                    # If the number can't be parsed as an integer, it's a subquestion
+                    except ValueError:
+                        answer_mapping = self.mapping[question_id].keys()
+                        for map in answer_mapping:
+                            component, attribute = map.split("/")
+                            try:
+                                self.components[component][attribute] = answer
+                            except KeyError:
+                                 print(f"Could not add attribute {attribute} to {component}, because {component} was not found")
+
+
+
 
 
 if __name__=="__main__":
