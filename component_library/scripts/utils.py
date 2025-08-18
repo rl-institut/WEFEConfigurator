441ea3426b25aa109a69beb89954d135b5a33eba
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-08-19 00:33:49 +0200

    Fix the mapping of components to resources/category in the library
    Each component is a row within a csv file, called a resource from the
    datapackage perspective. For adding components automatically it is
    important to know in which resource a component is located, thus this
    mapping provided by the function `utils.list_available_components()`.
    The module can be run standalone `python utils.py` to detect potential
    problems each time one introduces a new component into the library.

--- component_library/scripts/utils.py
+++ component_library/scripts/utils.py
@@ -11,6 +11,7 @@ import tableschema
 
 
 COMPONENT_TEMPLATES_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), "WIP_components"))
+print(COMPONENT_TEMPLATES_PATH)
 
 COMPONENTS_TYPEMAP = {
     "apv-system": MIMO,
@@ -45,28 +46,33 @@ def list_available_components():
 
     component_to_csv_name_mappping = {}
     for r in p0.resources:
-        logging.debug(r.name)
-        category = r.name
-        try:
-            resource_data = pd.DataFrame.from_records(r.read(keyed=True))
-        except tableschema.exceptions.CastError as err:
-            if err.errors:
-                logging.error(f"The resource {category} has the following casting errors: {','.join([str(e) for e in err.errors])}")
-            else:
-                logging.error(f"The resource {category} has the following casting error: {err}")
-            resource_data = pd.DataFrame()
-
-        if resource_data.empty is False:
-            if len(resource_data.columns) == 1:
-                logging.warning(f"The resource {category} has only one field detected, this is usually the case when there is a mismatch of number of values between the headers row and the data rows, please check your file.")
-            for component_name in resource_data.name.values:
-                if component_name not in component_to_csv_name_mappping:
-                    component_to_csv_name_mappping[component_name] = category
+        logging.info(r.name)
+        if os.sep + "elements" + os.sep in r.descriptor["path"]:
+            category = r.name
+            try:
+                resource_data = pd.DataFrame.from_records(r.read(keyed=True))
+            except tableschema.exceptions.CastError as err:
+                if err.errors:
+                    logging.error(f"The resource {category} has the following casting errors: {','.join([str(e) for e in err.errors])}")
                 else:
-                    raise ValueError(
-                        f"The component {component_name} is listed under several categories: {component_to_csv_name_mappping[component_name]} and {category}")
-        else:
-            logging.warning(f"The resource {category} is empty")
+                    logging.error(f"The resource {category} has the following casting error: {err}")
+                resource_data = pd.DataFrame()
+
+            if resource_data.empty is False:
+                if len(resource_data.columns) == 1:
+                    logging.warning(f"The resource {category} has only one field detected, this is usually the case when there is a mismatch of number of values between the headers row and the data rows, please check your file.")
+
+                if category == "profiles":
+                    import pdb;pdb.set_trace()
+
+                for component_name in resource_data.name.values:
+                    if component_name not in component_to_csv_name_mappping:
+                        component_to_csv_name_mappping[component_name] = category
+                    else:
+                        raise ValueError(
+                            f"The component {component_name} is listed under several categories: {component_to_csv_name_mappping[component_name]} and {category}")
+            else:
+                logging.warning(f"The resource {category} is empty")
     return component_to_csv_name_mappping
 
 AVAILABLE_COMPONENTS = list_available_components()
