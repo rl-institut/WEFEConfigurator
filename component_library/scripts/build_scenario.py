e9aad9f964c358eb73e58e6e38d88a2f111661d1
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-08-19 17:59:32 +0200

    Copy the busses from the component library thanks to foreignKeys

--- component_library/scripts/build_scenario.py
+++ component_library/scripts/build_scenario.py
@@ -1,10 +1,12 @@
 import os
 from copy import deepcopy
 import datapackage as dp
+import tableschema
 import numpy as np
 import pandas as pd
 import logging
 import json
+import shutil
 
 from utils import AVAILABLE_COMPONENTS, COMPONENT_TEMPLATES_PATH
 from analyse_survey import create_components_list
@@ -45,8 +47,9 @@ with open(os.path.join(project_dir, "app","sub_question_mapping.json"),"r") as f
 
 
 class ScenarioBuilder:
-    def __init__(self, name="test_scenario"):
+    def __init__(self, name="test_scenario", overwrite=False):
         self.name = name # should be updated based on scenario
+        self.overwrite = overwrite
         self.mapping = SURVEY_ANSWER_COMPONENT_MAPPING
         self.subq_mapping = SUB_QUESTION_MAPPING
         self.components = {}
@@ -57,9 +60,14 @@ class ScenarioBuilder:
     def create_scenario_folder(self, destination_path=scenario_dir):
         """Create a folder with the datapackage structure, the components and timeseries will be filled later on"""
         scenario_folder = os.path.join(destination_path, self.name)
+        create_folder = True
         if os.path.exists(scenario_folder):
-            pass
-        else:
+            if self.overwrite is False:
+                create_folder = False
+            else:
+                shutil.rmtree(scenario_folder)
+
+        if create_folder is True:
             os.makedirs(scenario_folder)
             os.makedirs(os.path.join(scenario_folder, "scripts"))
             os.makedirs(os.path.join(scenario_folder, "data", "elements"))
@@ -67,7 +75,7 @@ class ScenarioBuilder:
 
         return scenario_folder
 
-    def water_systems(self, survey):
+    def water_systems_postprocessing(self, survey):
         """Go through the survey and implement specific logic regarding the water questions"""
         water_distinction_question_id = "2"
         # TODO look answer to question 2 and implement specific logic there
@@ -75,6 +83,24 @@ class ScenarioBuilder:
         # need to pay attention if the question numbering changes to also carry out the changes here
         # This is a tradeoff between efficiency of survey processing and being able to treat special cases as we would
         # like to and make conditional choices (like add this component only to the drinking water bus and only if quesiton XYZ was answered with ...)
+
+    @property
+    def reference_datapackage(self):
+        dp_json = os.path.join(COMPONENT_TEMPLATES_PATH, "datapackage.json")
+        return dp.Package(dp_json)
+
+    @property
+    def scenario_datapackage(self):
+        dp_json = os.path.join(self.scenario_folder, "datapackage.json")
+        if os.path.exists(dp_json):
+            answer = dp.Package(dp_json)
+        else:
+            answer = dp.Package(base_path=self.scenario_folder)
+        return answer
+
+    @property
+    def scenario_component_folder(self):
+        return os.path.join(self.scenario_folder, "data", "elements")
 
     def process_survey(self, survey):
         """
@@ -167,11 +193,8 @@ class ScenarioBuilder:
         attributes will be updated
         """
 
-        scenario_component_folder = os.path.join(self.scenario_folder)
-
-        dp_json = os.path.join(COMPONENT_TEMPLATES_PATH, "datapackage.json")
-        dp_ref = dp.Package(dp_json)
-        dp_new = dp.Package(base_path=scenario_component_folder)
+        dp = self.scenario_datapackage
+        dp_ref = self.reference_datapackage
         for component in self.components:
             if component in AVAILABLE_COMPONENTS:
                 # Load the resource from the reference datapackage
@@ -180,10 +203,11 @@ class ScenarioBuilder:
 
                 # Strip the component documentation columns
                 selected_columns = [col for col in df.columns if col not in ['verbose_name', 'description']]
+
                 component_params = df.loc[component]
                 component_params = component_params[selected_columns]
 
-                ofname = os.path.join(scenario_component_folder, "data", "elements", f"{AVAILABLE_COMPONENTS[component]}.csv")
+                ofname = os.path.join(self.scenario_component_folder, f"{AVAILABLE_COMPONENTS[component]}.csv")
 
                 # Write or modify the component in the new datapackage
                 if os.path.exists(ofname):
@@ -197,24 +221,30 @@ class ScenarioBuilder:
                         component_params = category_df.loc[component]
                         category_df.loc[component] = self.update_component_attributes(component_params)
                     # Save the components back to the csv file
-                    category_df.to_csv(ofname, index_label="name")
+                    category_df.to_csv(ofname, index_label="name", sep=";")
 
 
                 else:
                     # Copy package metadata
                     descriptor = deepcopy(resource.descriptor)
-                    dp_new.add_resource(descriptor)
-                    dp_new.commit()
+                    selected_fields = []
+                    for f in descriptor["schema"]["fields"]:
+                        if f["name"] in selected_columns + ["name"]:
+                            selected_fields.append(f)
+                    print(selected_fields)
+                    descriptor["schema"]["fields"] = selected_fields
+                    dp.add_resource(descriptor)
+                    dp.commit()
 
                     # Edit the attributes in the csv file if they have been set in the survey
                     component_params = self.update_component_attributes(component_params)
                     component_df = component_params.to_frame().T
-                    component_df.to_csv(ofname, index_label="name")
+                    component_df.to_csv(ofname, index_label="name", sep=";")
 
             else:
                 logging.warning(f"The component {component} is not in the available component list {', '.join([comp for comp in AVAILABLE_COMPONENTS])}")
 
-        dp_new.save(os.path.join(scenario_component_folder, "datapackage.json"))
+        dp.save(os.path.join(self.scenario_folder, "datapackage.json"))
 
     def update_component_attributes(self, component_params):
         """
@@ -240,7 +270,7 @@ class ScenarioBuilder:
         for renewable energy output) will be used.
         :param custom_timeseries: DataFrame with datetime index and elements as columns (should be uploaded as .csv or .xlsx
         """
-        scenario_component_folder = os.path.join(self.scenario_folder, "data/elements")
+        scenario_component_folder = self.scenario_component_folder
         scenario_sequences_folder = os.path.join(self.scenario_folder, "data/sequences")
 
         if not os.path.exists(scenario_component_folder):
@@ -282,7 +312,7 @@ class ScenarioBuilder:
 
                     # Save sequences to .csv file
                     filepath = os.path.join(scenario_sequences_folder, sequences_filename)
-                    profile_df.to_csv(filepath)
+                    profile_df.to_csv(filepath,sep=";")
 
         # TODO check the foreign keys between timeseries and component attributes are valid
         # i.e. that each of the component attribute value correspond to a timeseries header
@@ -294,28 +324,69 @@ class ScenarioBuilder:
         Adds a bus.csv file to elements containing all the necessary buses. Looks through existing components to check
         which buses should be in the system.
         """
-        scenario_component_folder = os.path.join(self.scenario_folder, "data/elements")
+        scenario_component_folder = self.scenario_component_folder
+
+        dp_ref = self.reference_datapackage
+        ref_buses = dp_ref.get_resource("bus")
+        df_ref_buses = pd.DataFrame.from_records(ref_buses.read(keyed=True))
+
+        dp = self.scenario_datapackage
 
         if not os.path.exists(scenario_component_folder):
             logging.warning("No components found to infer buses. Please add components to the system first.")
 
         else:
-            buses = []
-            for filename in os.listdir(scenario_component_folder):
-                file = os.path.join(scenario_component_folder, filename)
-                components = pd.read_csv(file)
-                # Look for columns pertaining to bus connections
-                # TODO use the datapakage to get the busses connection
-                bus_cols = components.filter(regex="^(bus|from_bus_.*|to_bus_.*)$").columns
-                for col in bus_cols:
-                    buses.extend(components[col].tolist())
+            buses_to_add = []
+            for res in dp.resources:
+                if (os.sep + "elements" + os.sep in res.descriptor["path"]) and res.name != "bus":
+                    try:
+                        resource_data = pd.DataFrame.from_records(res.read(keyed=True))
+                    except tableschema.exceptions.CastError as err:
+                        if err.errors:
+                            logging.error(
+                                f"The resource {res.name} has the following casting errors: {','.join([str(e) for e in err.errors])}")
+                        else:
+                            logging.error(f"The resource {res.name} has the following casting error: {err}")
+                        resource_data = pd.DataFrame()
+                    print(res.name)
+                    print()
+                    for fk in res.descriptor["schema"]["foreignKeys"]:
+                        if fk["reference"]["resource"] == "bus":
+                            col_name = fk["fields"]
+                            if col_name in resource_data.columns:
+                                bus_names = resource_data[col_name].values.tolist()
+                                # check the bus names are listed in the component library
+                                for bus_name in bus_names:
+                                    if bus_name not in df_ref_buses.name.values:
+                                        raise KeyError(f"In the column '{col_name}' of the resource '{res.name}' the bus {bus_name} is listed, however it is missing from the component library resource 'bus.csv'")
+
+                                buses_to_add.extend(bus_names)
+                            else:
+                                logging.error(f"Column '{col_name}' missing from resource '{res.name}' although it is listed as foreignKey")
+                print(buses_to_add)
+
+            # for filename in os.listdir(scenario_component_folder):
+            #     file = os.path.join(scenario_component_folder, filename)
+            #     components = pd.read_csv(file)
+            #     # Look for columns pertaining to bus connections
+            #     # TODO use the datapakage to get the busses connection
+            #     bus_cols = components.filter(regex="^(bus|from_bus_.*|to_bus_.*)$").columns
+            #     for col in bus_cols:
+            #         buses_to_add.extend(components[col].tolist())
 
             # Convert to set to keep only unique values (add each bus once)
-            buses = list(set(buses))
-            # Save to buses .csv file
-            filepath = os.path.join(scenario_component_folder, "bus.csv")
-            bus_df = pd.DataFrame({"bus": buses, "type": "bus", "balanced": "True"})
-            bus_df.to_csv(filepath, index=False)
+            buses_to_add = list(set(buses_to_add))
+
+
+            df_buses = []
+            print(df_ref_buses)
+
+            for bus_name in buses_to_add:
+                df_buses.append(df_ref_buses.loc[df_ref_buses.name == bus_name])
+            print(df_buses)
+            ofname = os.path.join(scenario_component_folder, "bus.csv")
+            df_buses = pd.concat(df_buses)
+            df_buses.to_csv(ofname, index=False, sep=";")
 
 
     def fetch_component_timeseries(self, component):
@@ -334,11 +405,17 @@ if __name__=="__main__":
     repo_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "scenarios")
     # create_scenario_from_survey_data({}, "test_scenario", repo_path)
 
-    with open(os.path.join(project_dir, "app", "test_question3.json"), "r") as fp:
+    scen_id = 7
+
+    with open(os.path.join(project_dir, "app", f"scenario_{scen_id}_survey_answers.json"), "r") as fp:
         survey_answers =  json.load(fp)
 
-    scenario = ScenarioBuilder(name="my_test_scenario")
+    scenario = ScenarioBuilder(name=f"scenario_{scen_id}", overwrite=True)
+    #parse the survey to add components to a list
     scenario.process_survey(survey_answers)
+    #scenario.water_systems_postprocessing()
+    # adding the component to the datapackge from the component library based on the list of component
+    # to add we got from the survey
     scenario.add_components()
     scenario.add_buses()
     scenario.add_sequences()
