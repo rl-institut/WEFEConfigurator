5ce647c623ec9c1bf7edb264ea4f921345ecaa0f
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2025-08-20 00:28:16 +0200

    Distinguish between inferring foreignKeys and building datapackage.json

--- component_library/scripts/validate_component_lib.py
+++ component_library/scripts/validate_component_lib.py
@@ -4,7 +4,20 @@ import os
 
 COMPONENT_TEMPLATES_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), "WIP_components"))
 
-building.infer_metadata_from_data(
-    package_name="WEFE component library",
-    path=COMPONENT_TEMPLATES_PATH,
-)
+dp_json = os.path.join(COMPONENT_TEMPLATES_PATH, "datapackage.json")
+
+if os.path.exists(dp_json):
+    print("Only inferring metadata")
+    p = dp.Package(dp_json)
+    building.infer_package_foreign_keys(p)
+    p.descriptor["resources"].sort(key=lambda x: (x["path"], x["name"]))
+    p.commit()
+    p.save(dp_json)
+
+else:
+    print("Creating datapackage.json")
+    building.infer_metadata_from_data(
+        package_name="WEFE component library",
+        path=COMPONENT_TEMPLATES_PATH,
+    )
+
