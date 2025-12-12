717d89a1cd93a2b59202bd67721b3586294dd84a
Vivek-H-Rana <vivekhrana24@gmail.com>
2025-12-12 16:42:19 +0100

    Fix build_scenario.py: correct water/wastewater postprocessing and waste data import

--- component_library/scripts/build_scenario.py
+++ component_library/scripts/build_scenario.py
@@ -61,6 +61,7 @@ class ScenarioBuilder:
         self.additional_busses = []
         self.weather_data_path = "weather_data.csv"
         self.demand_data_path = "demand_data.csv"
+        self.waste_data_path = "waste_data.csv"
         self.scenario_folder = self.create_scenario_folder()
 
 
@@ -213,6 +214,24 @@ class ScenarioBuilder:
                 if comp_key in efficiency_values:
                     self.components[comp_key].update({"efficiency": efficiency_values[comp_key]})
 
+        def add_excess():
+            for component_key in list(self.components.keys()):
+                component_type, component_name = component_key
+                if component_type == "biological_denitrification":
+                    self.add_single_component(component_type="excess-N2")
+                if component_type == "biofiltration":
+                    self.add_single_component(component_type="excess-biomass")
+                if component_type in {
+                    "microfiltration",
+                    "ultrafiltration",
+                    "nanofiltration",
+                    "electrodialysis",
+                    "distillation",
+                    "membrane_distillation",
+                    "reverse_osmosis"
+                }:
+                    self.add_single_component(component_type="brine-excess")
+
         safety_check()
         if self.criterias["2"] == "Yes": # set Yes currently
             suffixes_a = ["_GWa", "_DSa", "_RCa", "_La"] # drinking water
@@ -256,7 +275,7 @@ class ScenarioBuilder:
             drinking_water_component_list = list(dict.fromkeys(drinking_water_component_list))
             print("DW treatment dictionary")
             drinking_water_component_list = arrange_components(water_treatment_train["main_list"], drinking_water_component_list)
-            drinking_water_treatment_dict = create_component_dict(drinking_water_component_list, entry_bus="water-in-bus", water_type="drinking")
+            drinking_water_treatment_dict = create_component_dict(drinking_water_component_list, entry_bus="groundwater-bus", water_type="drinking")
             for (component_type, component_name), component_attrs in drinking_water_treatment_dict.items():
                 # Add the component from the train
                 self.add_single_component(component_type, component_name, component_attrs)
@@ -267,12 +286,14 @@ class ScenarioBuilder:
             update_component_parameters(suffixes, "DW")
             #print(drinking_water_treatment_dict)
 
+        add_excess()
+
     def waste_water_systems_postprocessing(self, survey):
 
         def safety_check():
             # --- SAFETY CLEANUP STEP ---
             # Remove any existing wastewater-treatment components that process_survey might have added
-            for comp in ["septic_system", "constructed_wetland", "centralized_waste_water_treatment_plant", "decentralized_waste_water_treatment_plant", "water_reuse_system"]:
+            for comp in ["septic_system", "constructed_wetland", "centralized_WWTP", "decentralized_WWTP", "water_reuse_system"]:
                 self.components.pop((comp, comp), None)
             # --- END CLEANUP ---
 
@@ -309,6 +330,7 @@ class ScenarioBuilder:
                             "water_out_bus": "wwtp-ip-water-bus"
                         }
                     )
+                    #self.add_single_component(component_type="excess-biomass")
                     capacity = survey["criteria_7.1.0"]
                     if capacity not in (None, "", " "):
                         self.components[component_key].update({"capacity": capacity})
@@ -338,6 +360,7 @@ class ScenarioBuilder:
                         "water_out_bus": "wwtp-ip-water-bus"
                     }
                 )
+                #self.add_single_component(component_type="excess-biomass")
                 capacity = survey["criteria_7.1.0"]
                 if capacity not in (None, "", " "):
                     self.components[component_key].update({"capacity": capacity})
@@ -346,12 +369,13 @@ class ScenarioBuilder:
             if population >= 10000:
                 # centralized waste water treatment plant
                 component_key = self.add_single_component(
-                    component_type="centralized_waste_water_treatment_plant",
+                    component_type="centralized_WWTP",
                     component_attrs={
                         "water_in_bus": "wwtp-ip-water-bus",
                         "water_out_bus": "wwtp-op-water-bus"
                     }
                 )
+                #self.add_single_component(component_type="excess-biomass")
                 capacity = survey["criteria_7.1.2"]
                 if capacity not in (None, "", " "):
                     self.components[component_key].update({"capacity": capacity})
@@ -359,12 +383,13 @@ class ScenarioBuilder:
                 # decentralized waste water treatment plant
                 # default addition of this component
                 component_key = self.add_single_component(
-                    component_type="decentralized_waste_water_treatment_plant",
+                    component_type="decentralized_WWTP",
                     component_attrs={
                         "water_in_bus": "wwtp-ip-water-bus",
                         "water_out_bus": "wwtp-op-water-bus"
                     }
                 )
+                #self.add_single_component(component_type="excess-biomass")
                 capacity = survey["criteria_7.1.3"]
                 if capacity not in (None, "", " "):
                     self.components[component_key].update({"capacity": capacity})
@@ -378,6 +403,7 @@ class ScenarioBuilder:
                     "water_out_bus": "service-water-bus"
                 }
             )
+            self.add_single_component(component_type="hh_gw_waste")
             capacity = survey["criteria_7.1.4"]
             if capacity not in (None, "", " "):
                 self.components[component_key].update({"capacity": capacity})
@@ -536,6 +562,20 @@ class ScenarioBuilder:
             self.download_weather_data()
 
         return pd.read_csv(self.weather_data_path)
+
+    def download_waste_data(self):
+        if not os.path.exists(self.waste_data_path):
+            # TODO: empty df for now, add real data download later
+            df = pd.DataFrame(columns=["dummy"])
+            df.to_csv(self.waste_data_path, index=False)
+
+    @property
+    def waste_data(self):
+        if not os.path.exists(self.waste_data_path):
+            self.download_waste_data()
+
+        # TODO: test this function to see handling of different csv formats
+        return pd.read_csv(self.waste_data_path, delimiter=",", quotechar='"', decimal=",")
 
     # TODO: put together demand_data, weather_data and others to profiles_data or sequences_data
     #  and process all together as one df
@@ -891,7 +931,7 @@ class ScenarioBuilder:
                                 logging.error(
                                     f"Column '{col_name}' missing from resource '{res.name}' although it is listed as foreignKey")
             # WIP, here need to take an external file as argument and only select 'profiles_to_add' columns
-
+            print(profiles_to_add)
             if len(profiles_to_add) == 0:
                 print(f"No profiles listed within the component for the '{self.scenario_folder.split(os.sep)[-1]}' datapage. If you think it is an error, double check the foreign keys")
             else:
@@ -902,19 +942,30 @@ class ScenarioBuilder:
                 # Get demand data
                 demand_df = self.demand_data
 
+                # Get waste data
+                waste_df = self.waste_data
+
                 # Check data profiles length
-                # TODO: Check possible issues with demand data and weather data,
-                #  I assumed weather data to always be of correct length while demand data may be flexible in length
+                # TODO: Check possible issues with demand data, weather data and waste data
+                #  I assumed waste data to always be of correct length while demand data or weather data may be flexible in length
                 if len(weather_df) != len(demand_df):
                     logging.warning(f"Length mismatch between {self.demand_data_path} and {self.weather_data_path}.")
+                if len(demand_df) != len(waste_df):
+                    logging.warning(f"Length mismatch between {self.demand_data_path} and {self.waste_data_path}.")
+                if len(weather_df) != len(waste_df):
+                    logging.warning(f"Length mismatch between {self.waste_data_path} and {self.weather_data_path}")
 
-                if demand_df.dropna(how="all").empty:
-                    profiles_len = len(weather_df)
-                    logging.warning(f"{self.demand_data_path} seems to be effectively empty")
-                    logging.info(f"{self.weather_data_path} will be used to set the time index of the model.")
+                if waste_df.dropna(how="all").empty:
+                    if demand_df.dropna(how="all").empty:
+                        profiles_len = len(weather_df)
+                        logging.warning(f"{self.demand_data_path} seems to be effectively empty")
+                        logging.info(f"{self.weather_data_path} will be used to set the time index of the model.")
+                    else:
+                        profiles_len = len(demand_df)
+                        logging.info(f"{self.demand_data_path} will be used to set the time index of the model.")
                 else:
-                    profiles_len = len(demand_df)
-                    logging.info(f"{self.demand_data_path} will be used to set the time index of the model.")
+                    profiles_len = len(waste_df)
+                    logging.info(f"{self.waste_data_path} will be used to set the time index of the model.")
 
                 # Get blueprint profile names from component library for mapping
                 lib_profiles_path = os.path.join(lib_dir, "WIP_components", "data", "sequences", "profiles.csv")
@@ -947,6 +998,8 @@ class ScenarioBuilder:
                             scen_profiles_df[profile] = weather_df[matching_col].reindex(range(profiles_len)).values
                         elif matching_col in demand_df.columns:
                             scen_profiles_df[profile] = demand_df[matching_col].reindex(range(profiles_len)).values
+                        elif matching_col in waste_df.columns:
+                            scen_profiles_df[profile] = waste_df[matching_col].reindex(range(profiles_len)).values
                         else:
                             logging.warning(
                                 f"Profile '{profile}' is not in the available data. A dummy profile (series of 1) will be used.")
@@ -1060,8 +1113,7 @@ class ScenarioBuilder:
                                 for bus_name in bus_names:
                                     if bus_name not in df_ref_buses.name.values:
                                         if bus_name not in self.additional_busses:
-                                            if bus_name is not None: # TODO: hack to allow null buses in water csv
-                                                raise KeyError(f"In the column '{col_name}' of the resource '{res.name}' the bus {bus_name} is listed, however it is missing from the component library resource 'bus.csv'")
+                                            raise KeyError(f"In the column '{col_name}' of the resource '{res.name}' the bus {bus_name} is listed, however it is missing from the component library resource 'bus.csv'")
 
                                 buses_to_add.extend(bus_names)
                             else:
